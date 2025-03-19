1af44d968908019ad70aaac1a5fe733c81db7364
william <emelifonwuw@gmail.com>
2025-03-19 06:11:35 +0100

    fix: vite config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
+import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
-  plugins: [react()],
-  server:{
-    host: true,
+  plugins: [
+    react(), tailwindcss(),
+  ],
+  server: {
+    host: '0.0.0.0',
     port: 3000,
+    strictPort: true,
+    allowedHosts: ['online-clearance.onrender.com'] 
+  },
+  preview: {
+    allowedHosts: ['online-clearance.onrender.com'] 
   }
 })
