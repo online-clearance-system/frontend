d371adc65dbf574fd6376f252d5ba5fd478ad00d
Treasure Aiyedun <treasureisatechsis@gmail.com>
2025-03-24 09:16:50 +0100

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server:{
-    host: true,
+  plugins: [
+    react(),
+  ],
+  server: {
+    host: '0.0.0.0',
     port: 3000,
+    strictPort: true,
+    allowedHosts: ['online-clearance.onrender.com'] 
+  },
+  preview: {
+    allowedHosts: ['online-clearance.onrender.com'] 
   }
 })
